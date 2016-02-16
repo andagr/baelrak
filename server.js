281e969b1b49e2d07c556a0f6c89ebcd01077fe7
Andreas Ågren <bandagr@gmail.com>
2016-02-16 22:12:46 +0100

    Added Procfile and changed port

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
     response.end('<html><body><h1>Hello!</h1></body></html>');
-}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 5000);
