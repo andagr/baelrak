4c793cff4895b2de76bef41d03a6215bf3249097
Andreas Ågren <bandagr@gmail.com>
2016-02-16 21:13:30 +0100

    hello world

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
-});
+    response.end('<html><body><h1>Hello!</h1></body></html>');
+}).listen(process.env.PORT || 8080);
