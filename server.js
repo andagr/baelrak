7007c51cac124e985dfd7079353ef6c81a2f8046
Andreas Ågren <bandagr@gmail.com>
2016-02-20 13:22:56 +0100

    test deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('<html><body><h1>Hello again!</h1></body></html>');
+    res.send('<html><body><h1>Hello again again!</h1></body></html>');
 });
 
 app.listen(process.env.PORT || 5000, function () {
