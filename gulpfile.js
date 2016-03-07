f2e449dfca4697d357eb56cdb2ef25848c7015e8
Andreas Ågren <bandagr@gmail.com>
2016-03-07 21:27:23 +0100

    build app with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('default', () => {
-    
+gulp.task('default', ['clean', 'build']);
+
+gulp.task('clean', () => {
+    return del([
+        'static/**/*'
+    ]);
+});
+
+gulp.task('build', () => {
+    gulp.src('app/**/*')
+        .pipe(gulp.dest('static'))
 })
