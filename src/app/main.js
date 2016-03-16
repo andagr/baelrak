dd42bedc368c96d5477449a0b41a708dae1ec4f7
Andreas Ågren <bandagr@gmail.com>
2016-03-16 23:37:37 +0100

    Added bunny! (Fixed aspect ratio for renderer.)

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,25 @@
 import PIXI from 'pixi.js'
 
-console.log(PIXI);
+let ratio = 16/9;
+let height = Math.min($(global).height() * 0.8, $(global).width() / ratio);
+let width = height * ratio;
+let renderer = PIXI.autoDetectRenderer(width, height);
+$('#game-area').append(renderer.view);
+let stage = new PIXI.Container();
+let bunny;
+PIXI.loader.add('bunny', '/img/bunny.png').load((loader, resources) =>{
+    bunny = new PIXI.Sprite(resources.bunny.texture);
+    bunny.position.x = width / 2;
+    bunny.position.y = height / 2;
+    bunny.scale.x = 2;
+    bunny.scale.y = 2;
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+    stage.addChild(bunny);
+    animate();
+});
+function animate() {
+    requestAnimationFrame(animate);
+    bunny.rotation += 0.01;
+    renderer.render(stage);
+}
